b60161a610fe7d3c4d3d35975a920c2da03d6578
Mirabellier <75848@siswa.unimas.my>
2022-12-12 16:15:34 +0800

    Add AI completion

--- events/onMessage.js
+++ events/onMessage.js
@@ -1,6 +1,30 @@
 const prefix = process.env.PREFIX;
 
 module.exports = (client, message) => {
+	if (message.body.startsWith("@601137463940")) {
+		const prm = message.body.slice("@601137463940".length).trim();
+
+		const { Configuration, OpenAIApi } = require("openai");
+
+		const configuration = new Configuration({
+			apiKey: process.env.OPEN_AI_KEY,
+		});
+
+		const openai = new OpenAIApi(configuration);
+
+		async function main() {
+			const response = await openai.createCompletion({
+				model: "text-davinci-003",
+				prompt: prm,
+			});
+
+			console.log(response.data.choices);
+
+			client.reply(message.from, response.data.choices[0].text, message.id);
+		}
+
+		main();
+	}
 	if (!message.body.startsWith(prefix)) return;
 
 	const args = message.body.slice(prefix.length).trim().split(/ +/g);
